fd6ab7896070909b02f36116514b0d41a4cbc1eb
lmorais <leozonnn@gmail.com>
2021-01-17 18:47:31 -0300

    registration screen

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import {
+  Container,
+  Title,
+  BackToSign,
+  BackToSignText
+} from './styles';
 
-import { Container } from './styles';
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
 
-const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps='handled'
+          contentContainerStyle={{flex: 1}}
+        >
+          <Container>
+            <Image source={logoImg}/>
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="E-mail"/>
+            <Input name="email" icon="mail" placeholder="E-mail"/>
+            <Input name="password" icon="lock" placeholder="Senha"/>
+
+            <Button onPress={() => { console.log('press button ok') }}>Entrar</Button>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSign onPress={() => {navigation.goBack()}}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignText>Voltar para logon</BackToSignText>
+      </BackToSign>
+    </>
+  )
 }
 
-export default SignIn;
+export default SignUp;
