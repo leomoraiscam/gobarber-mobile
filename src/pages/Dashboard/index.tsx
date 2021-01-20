185ceb1478cc7f914e9efc3d701be4e30d570431
lmorais <leozonnn@gmail.com>
2021-01-20 07:26:50 -0300

    resolve fix in auth hook

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
+import api from '../../services/api';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar
+} from './styles';
+
+interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { user } = useAuth();
-  const { navigate } = useNavigation()
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
