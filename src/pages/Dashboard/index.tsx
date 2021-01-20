e95a32dbfb6389a88d03c74c3475281cea76d161
lmorais <leozonnn@gmail.com>
2021-01-20 07:19:16 -0300

    header of dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
+import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const { navigate } = useNavigation()
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {"\n"}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url || 'https://avatars0.githubusercontent.com/u/49538119?s=400&u=39a6291923942b4f7cc8fcb4bce259d116807701&v=4'}} />
+        </ProfileButton>
+      </Header>
+    </Container>
   )
 }
 
