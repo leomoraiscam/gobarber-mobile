0e73dfa0d5ef3ce6deb8db8553c546966ebbde23
lmorais <leozonnn@gmail.com>
2021-01-20 22:37:54 -0300

    created structure of appointments

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 import { useAuth } from '../../hooks/auth';
+import { Container, Header, BackButton, HeaderTitle, UserAvatar  } from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  const { signOut } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const { user } = useAuth();
+
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>
+          Cabeleireiros
+        </HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url || 'https://avatars0.githubusercontent.com/u/49538119?s=400&u=39a6291923942b4f7cc8fcb4bce259d116807701&v=4'}} />
+      </Header>
+    </Container>
   )
 }
 
