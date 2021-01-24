4edf45cbb6c4c557b422201b66a19d3873db3d91
lmorais <leozonnn@gmail.com>
2021-01-24 17:14:56 -0300

    creating the screen structure

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useAuth } from '../../hooks/auth';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title,Description , OkButton,  OkButtonText} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard'
+        }
+      ],
+      index: 0
+    })
+  }, [reset]);
 
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Icon name="check" size={80} color={'#04d361'}/>
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h com Leonardo</Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   )
 }
 
